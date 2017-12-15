35a03587d99bdace879aa32d0c68462f9b3f9540
Miguel Antonio Amores Bassante <miguel.amores@ute.edu.ec>
2017-12-15 16:09:38 -0500

    scrapping finished

--- app.js
+++ app.js
@@ -16,32 +16,49 @@ app.get('/scrape', function(req, res){
 
   var $ = cheerio.load(body);
   var hackerNews = [];
-
-  /*$('tr.athing:has(td.votelinks)').each(function( index ) {
+  //$('tr.athing:has(td.votelinks)').each(function( index ) {
+  $('tr.athing').each(function( index ) {
     var rank = $(this).find('td.title > span.rank').text().trim();
     var title = $(this).find('td.title > a').text().trim();
-    var link = $(this).find('td.title > a').attr('href');
-    fs.appendFileSync('hackernews.txt',rank + '\n' + title + '\n' + link + '\n');
-  });*/
+    //var link = $(this).find('td.title > a').attr('href');
+    hackerNews.push({rank: rank, title: title});
+    fs.appendFileSync('hackernews.txt',index + '\n' + rank + '\n' + title + '\n');
+  });
 
+  //$('td.subtext:has(span.score)').each(function( index ) {
+  $('td.subtext').each(function( index ) {
+    var score = $(this).find('span.score').text().trim();
+    var comments = $(this).find('a:contains("comments")').text().trim();
+    fs.appendFileSync('hackernews.txt',index + '\n' + score + '\n' + comments + '\n');
 
-  $('table.itemlist > tbody > tr').each(function( index ) {
-  
-      if ($(this).has('tr.athing')) {
-        var rank = $(this).find('tr.athing > td.title > span.rank').text().trim();
+    hackerNews.forEach(function(element, i) {
+      if (i == index) {
+        element.score = score;
+        element.comments = comments;
       }
-
-      var title = $(this).find('tr.athing > td.title > a').text().trim();
-      //var link = $(this).find('tr.athing > td.title > a').attr('href');
-
-      var score = $(this).find('tr > td.subtext > span.score').text().trim();
-      var comments = $(this).find('tr > td.subtext > a:contains("comments")').text().trim();
-      hackerNews.push({rank: rank, title: title, score: score, comments: comments});
-      fs.appendFileSync('hackernews.txt',rank + '\n' + title + '\n' + score + '\n' + comments + '\n');
-
-
+        console.log("index "+ i + "indexsubel "+ index);
+    });
 
   });
+
+
+  // $('table.itemlist > tbody > tr').each(function( index ) {
+  //
+  //     if ($(this).has('tr.athing')) {
+  //       var rank = $(this).find('tr.athing > td.title > span.rank').text().trim();
+  //     }
+  //
+  //     var title = $(this).find('tr.athing > td.title > a').text().trim();
+  //     //var link = $(this).find('tr.athing > td.title > a').attr('href');
+  //
+  //     var score = $(this).find('tr > td.subtext > span.score').text().trim();
+  //     var comments = $(this).find('tr > td.subtext > a:contains("comments")').text().trim();
+  //     hackerNews.push({rank: rank, title: title, score: score, comments: comments});
+  //     fs.appendFileSync('hackernews.txt',rank + '\n' + title + '\n' + score + '\n' + comments + '\n');
+  //
+  //
+  //
+  // });
 
   res.send(hackerNews);
 
