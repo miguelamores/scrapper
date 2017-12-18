0acb7a4c33998c65fd461fcdfc64e7f9ee50a45b
Miguel Amores <miguelamores@Miguels-MacBook-Pro.local>
2017-12-17 22:36:24 -0500

    filtering points and comments

--- app.js
+++ app.js
@@ -4,72 +4,81 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
+var hackerNews = [];
+request("https://news.ycombinator.com/news", function(error, response, body) {
+    if(error) {
+      console.log("Error: " + error);
+    }
+    console.log("Status code: " + response.statusCode);
+
+    var $ = cheerio.load(body);
+
+    //$('tr.athing:has(td.votelinks)').each(function( index ) {
+    $('tr.athing').each(function( index ) {
+      var rank = $(this).find('td.title > span.rank').text().trim();
+      var title = $(this).find('td.title > a').text().trim();
+      //var link = $(this).find('td.title > a').attr('href');
+      hackerNews.push({rank: rank, title: title});
+      //fs.appendFileSync('hackernews.txt',index + '\n' + rank + '\n' + title + '\n');
+    });
+
+    //$('td.subtext:has(span.score)').each(function( index ) {
+    $('td.subtext').each(function( index ) {
+      var score = $(this).find('span.score').text().trim();
+      var comments = $(this).find('a:contains("comments")').text().trim();
+      //fs.appendFileSync('hackernews.txt',index + '\n' + score + '\n' + comments + '\n');
+
+      hackerNews.forEach(function(element, i) {
+        //console.log(element.title +" length: " + element.title.split(" ").length);
+        if (i == index) {
+          element.score = score;
+          element.comments = comments;
+        }
+      });
+    });
+
+});
 
 
 
-  request("https://news.ycombinator.com/news", function(error, response, body) {
-  if(error) {
-    console.log("Error: " + error);
-  }
-  console.log("Status code: " + response.statusCode);
 
-  var $ = cheerio.load(body);
-  var hackerNews = [];
-  //$('tr.athing:has(td.votelinks)').each(function( index ) {
-  $('tr.athing').each(function( index ) {
-    var rank = $(this).find('td.title > span.rank').text().trim();
-    var title = $(this).find('td.title > a').text().trim();
-    //var link = $(this).find('td.title > a').attr('href');
-    hackerNews.push({rank: rank, title: title});
-    fs.appendFileSync('hackernews.txt',index + '\n' + rank + '\n' + title + '\n');
-  });
+app.get('/scrape/:filter', function(req, res){
+  console.log("filter: " + req.params.filter);
+  var hackerNewsCopy = hackerNews.slice(0);
+  if (req.params.filter == "more") {
 
-  //$('td.subtext:has(span.score)').each(function( index ) {
-  $('td.subtext').each(function( index ) {
-    var score = $(this).find('span.score').text().trim();
-    var comments = $(this).find('a:contains("comments")').text().trim();
-    fs.appendFileSync('hackernews.txt',index + '\n' + score + '\n' + comments + '\n');
-
-    hackerNews.forEach(function(element, i) {
+    hackerNewsCopy.forEach(function(element, i) {
       console.log(element.title +" length: " + element.title.split(" ").length);
-      if (i == index) {
-        element.score = score;
-        element.comments = comments;
+      if (element.title.split(" ").length <= 5) {
+        console.log("element: " +element.title);
+        hackerNewsCopy.splice(i, 1);
       }
     });
 
+    hackerNewsCopy.sort(function(a, b){
+      var scoreA = (a.score).match(/\d+/g);
+      var scoreB = (b.score).match(/\d+/g);
+      return scoreA - scoreB;
+    })
+  } else {
+    hackerNewsCopy.forEach(function(element, i) {
+      console.log(element.title +" length: " + element.title.split(" ").length);
+      if (element.title.split(" ").length > 5) {
+        console.log("element: " +element.title);
+        hackerNewsCopy.splice(i, 1);
+      }
+    });
+
+    hackerNewsCopy.sort(function(a, b){
+      var commentsA = (a.comments).match(/\d+/g);
+      var commentsB = (b.comments).match(/\d+/g);
+      return commentsA - commentsB;
+    })
+  }
 
 
-  });
-
-
-  // $('table.itemlist > tbody > tr').each(function( index ) {
-  //
-  //     if ($(this).has('tr.athing')) {
-  //       var rank = $(this).find('tr.athing > td.title > span.rank').text().trim();
-  //     }
-  //
-  //     var title = $(this).find('tr.athing > td.title > a').text().trim();
-  //     //var link = $(this).find('tr.athing > td.title > a').attr('href');
-  //
-  //     var score = $(this).find('tr > td.subtext > span.score').text().trim();
-  //     var comments = $(this).find('tr > td.subtext > a:contains("comments")').text().trim();
-  //     hackerNews.push({rank: rank, title: title, score: score, comments: comments});
-  //     fs.appendFileSync('hackernews.txt',rank + '\n' + title + '\n' + score + '\n' + comments + '\n');
-  //
-  //
-  //
-  // });
-  hackerNews.sort(function(a, b){
-    var scoreA = (a.score).match(/\d+/g);
-    var scoreB = (b.score).match(/\d+/g);
-    return scoreA - scoreB;
-  })
-
-  res.send(hackerNews);
-
-});
+  res.send(hackerNewsCopy);
+  hackerNewsCopy = [];
   //All the web scraping magic will happen here
 
 })
